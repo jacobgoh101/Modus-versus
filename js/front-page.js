5bdb321c77fcc1fe3a187627c65782fe6b0bbc30
Jacob Goh Chuan Ching <jacobgoh101@gmail.com>
2015-11-28 00:08:12 +0800

    front-page out client section done. front-page done!

--- js/front-page.js
+++ js/front-page.js
@@ -9,6 +9,7 @@
 		});
 	});
 
+	// number counter
 	var number_counter_done = false;
 	$(window).scroll(function() {
 		if ($(this).scrollTop() > $(".about-why img").position().top && !number_counter_done) {
@@ -25,6 +26,45 @@
 			});
 			number_counter_done = true;
 		}   
+	});
+
+	// out client carousal
+	$(function(){
+		$('.our-client').slick({
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 2000,
+			responsive: [
+			{
+				breakpoint: 900,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					autoplay: true,
+					autoplaySpeed: 2000,
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					autoplay: true,
+					autoplaySpeed: 2000,
+				}
+			},
+			{
+				breakpoint: 320,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					autoplay: true,
+					autoplaySpeed: 2000,
+				}
+			}]
+
+		});
 	});
 
 })( jQuery );
